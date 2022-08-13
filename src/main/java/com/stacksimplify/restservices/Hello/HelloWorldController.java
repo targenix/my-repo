114714d9f7142222f5ac1af8abe1c8bd13f1b17e
ARCHANA SINHA <archanasinha1841@gmail.com>
2022-08-13 16:55:54 +0530

    i18n - first commit

--- src/main/java/com/stacksimplify/restservices/Hello/HelloWorldController.java
+++ src/main/java/com/stacksimplify/restservices/Hello/HelloWorldController.java
@@ -1,12 +1,22 @@
 package com.stacksimplify.restservices.Hello;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 //Controller
 @RestController
 
 public class HelloWorldController {
+	
+	
+	@Autowired
+	private ResourceBundleMessageSource messageSource;
     //Simple Method
 	//URI - /helloworld
 	//GET
@@ -19,5 +29,17 @@ public class HelloWorldController {
 	public UserDetails helloWorldBean()
 	{
 		return new UserDetails("Archana","Sinha","Gaya");
+	}
+	
+	@GetMapping("/hello-int")
+	public String getMessagesinI18NFormat(@RequestHeader(name= "Accept-Language", required=false) String locale) {
+		
+		return messageSource.getMessage("label.hello",null, new Locale(locale));
+	}
+	
+	@GetMapping("/hello-int2")
+	public String getMessagesinI18NFormat2() {
+		
+		return messageSource.getMessage("label.hello",null, LocaleContextHolder.getLocale());
 	}
 }
